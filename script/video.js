e5acd25162f8c1bdbd09ad05c88b65553a2e6426
assignment-01 <ykhan9463@gmail.com>
2025-06-16 23:43:40 +0600

    create card for videos

--- script/video.js
+++ script/video.js
@@ -15,7 +15,7 @@ const loadVideos = () => {
   //fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error));
 };
 
@@ -36,5 +36,30 @@ const displayCategories = (categories) => {
   });
 };
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+   
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+
+    `;
+    videoContainer.append(card);
+  });
+};
 loadCategories();
 loadVideos();
